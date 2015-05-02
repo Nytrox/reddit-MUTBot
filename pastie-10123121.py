330d7032f01731bd9a79589e2a5530095b63493f
Henry <neonnytrox@gmail.com>
2015-05-01 18:35:33 -0700

    Giveaway & GiveawayWinner
    Added basis of Giveaway functions

--- pastie-10123121.py
+++ pastie-10123121.py
@@ -10,6 +10,37 @@ pullWords = ['snipe', 'pull', 'offer', '99', 'got', 'omg', 'quicksell', 'trio', 
 wasBriefed = []
 wasBriefedPull = []
 
+def Giveaway(players, system):
+    assert type(players) is StringType, "players is not an String: %r" % players
+    assert type(system) is StringType, "system is not an String: %r" % system
+    #Not sure if this works. I hope it's SubmitMixin.
+    r.SubmitMixin.submit(r.get_subreddit('MaddenUltimateTeam'), system + " Giveaway!",
+        text= "We are giving away " + players ". The system is " + system + "\n\n. Please comment once. Double submissions will be ignored completely. Also, your account must be older than 6 months"
+        , save = True)
+    r.set_flair(subreddit, submission, 'giveaway', 'giveaway')
+    #Need to somehow save the URL of the submission. I saved the submission, but I need to re-access the submission
+    URL = r.get_submission() #YOLO HOPE THIS WORKS
+    return URL
+    
+def GiveawayWinner(GiveawayURL):
+    l, WinnerFound = [], True
+    submission = r.get_submission(url=GiveawayURL, submission_id=None, comment_limit=None)
+    comments = submission.get_comments(‘all’) #get_all_comments apparently will be removed
+    #How the hell do I get the number of comments? I wish I could test this out...
+    for comment in comments:
+        #There's probably an easier way to do this, like only finding the roots of the forest
+        #Like maybe I can use praw.helpers.flatten_tree(tree, nested_attr=u'replies', depth_first=False)
+        if comment.is_root():
+            l.append(comment)
+    while WinnerFound = True:
+        WinnerNum = randint(0, l.size())
+        #Get Comment Author/redditor. No idea how to do that
+        Winner = l[WinnerNum].get_redditor() #totally fake/made up function
+        #Damn, looks like we need to do this to get the redditor age http://www.reddit.com/r/learnpython/comments/2txd3t/praw_question_regarding_redditor_account_age/
+        if redditor account age >= 6 months:
+            WinnerFound = False
+    return Winner
+    
 while True:
     subreddit = r.get_subreddit('MaddenUltimateTeam')
     for submission in subreddit.get_new(limit = 10):
@@ -43,7 +74,7 @@ while True:
                     r.set_flair(subreddit, submission, 'stream', 'stream')
                 #Scammer
                 elif any("scammer" in submission.title):
-                    r.set_flair(subreddit, submission, 'scammeralert', 'scammeralert')
+                    r.set_flair(subreddit, submission, 'SCAMMER ALERT', 'scammeralert')
                 elif submission.domain == 'imgur.com':
                     msg = submission.short_link
                     r.send_message('/r/CardsCreations', 'Possible Pull', msg)
