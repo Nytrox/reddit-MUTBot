0133ee861c56d07674b7b1cb9e928bd22573e8b9
TheCard26 <thecard26@gmail.com>
2015-05-05 00:26:17 -0400

    Update and rename pastie-10123121.py to MUTBot.py

--- MUTBot.py
+++ MUTBot.py
@@ -3,16 +3,17 @@ import time
 import random
 
 r = praw.Reddit(user_agent = '/u/TheCard\'s and /u/NeonNytrox\'s moderator for /r/MaddenUltimateTeam that removes posts meeting certain criteria')
-r.login('MUTMod', "901n324oiDDa/;'")
+r.login()
 
 alreadyModerated = []
 #TODO: ADD MORE HERE
-pullWords = ['snipe', 'pull', 'offer', '99', 'got', 'omg', 'quicksell', 'trio', 'badge', '...'] 
+pullWords = ['snipe', 'pull', 'offer', '99', 'got', 'omg', 'quicksell', 'trio', 'badge', '...', 'bang', 'boom'] 
 wasBriefed = []
 wasBriefedPull = []
+giveawayTimes, giveawayURLs = [], []
     
 def GiveawayWinner(GiveawayURL):
-    random.seed(time.clock())
+    random.seed(time.time())
     l, WinnerFound, numbOfComments, prevAuthors = [], False, 0, []
     submission = r.get_submission(url=GiveawayURL, submission_id=None, comment_limit=None)
     comments = submission.comments #first parameter in get_comments is for the subreddit. Instead we want comments in the submission
@@ -29,21 +30,45 @@ def GiveawayWinner(GiveawayURL):
     while WinnerFound == False:
         WinnerNum = random.randint(0, numbOfComments-1)
         #Gets the author of the winning comment
-        Winner = r.get_redditor(l[WinnerNum])
+        Winner = l[WinnerNum]
         #To find account age, initialize a user object and call the variable 'created'.
         #Variable is returned in seconds since Epoch, so we need to have the six months in Seconds since Epoch too
         sixMonths = 15552000
         if Winner.created - sixMonths >= 0:
             WinnerFound = True
         
-        return Winner
+        r.send_message(Winner, 'Congrats', 'You\'ve won the giveaway! Please contact the giveaways author to receive your prize; we\'ll let them know you won.')
+        time.sleep(2) #Ugh so many disconnects
+        msg = Winner + 'has won your giveaway. Please message them and arrange the card exchange. They have been notified as well.'
+        r.send_message(submission.author, 'Your giveaway winner', msg)
     
 def checkInbox():
-    #PlaceHolder so we don't get errors for an empty method
-    return True
+    #Variable to store PMs in
+    pms = None
+    #Get all unread messages
+    pms = r.get_unread(limit = none)
+    
+    if pms is not None:
+        for pm in pms:
+            #Check for giveaway replies
+            if 'Giveaway' in pm.subject:
+                giveawayURL = pm.[9:]
+                #see if they're saying they want mutmod to handle their giveaway
+                if pm.body.lower() == 'y' or pm.body.lower() == 'yes':
+                    pm.reply('Great! Please now reply the amount of hours you want the giveaway to end in. Please reply with the digit only, and keep the rest of the PM empty. Thank you.')
+                #See if the reply is a digit, i.e. a time
+                if pm.body.isdigit():
+                    #Set a time (in seconds since epoch) for giveaway bot to run (3600 seconds == 1 hour)
+                    giveawayTimes.append(int(pm.body) * 3600 + time.time())
+                    giveawayURLs.append(giveawayURL)
+            else:
+                #Forward every message to the modmail of the subreddit
+                r.send_message('/r/MaddenUltimateTeam', 'Message', pm.body)
+            time.sleep(2)
+            pm.mark_as_read()
     
 while True:
-    subreddit = r.get_subreddit('mutmod')
+    subreddit = r.get_subreddit('MaddenUltimateTeam')
     for submission in subreddit.get_new(limit = 10):
         if submission.id not in alreadyModerated:
             #Welcome new members
@@ -61,9 +86,7 @@ while True:
                 #Giveaway
                 elif "giveaway" in submission.title.lower() and "thanks" not in submission.title.lower():
                     r.set_flair(subreddit, submission, 'giveaway', 'giveaway')
-                    r.send_message(submission.author, 'Giveaway ' + submission.short_link, 'Do you want us to handle your giveaway for you? Reply with a "y" for yes. Otherwise, no need to reply. To learn more, read [here](www.reddit.com/r/MUTMod/wiki.giveaway)')
-                    #GiveawayWinner returns the winner, so we just call the method and put that as the recipient
-                    r.send_message(GiveawayWinner(submission.url), 'Congrats', 'You won the giveaway')
+                    r.send_message(submission.author, 'Giveaway ' + submission.url, 'Do you want us to handle your giveaway for you? Reply with a "y" for yes. Otherwise, no need to reply. To learn more, read [here](www.reddit.com/r/MUTMod/wiki.giveaway)')
                 #Review
                 elif "review" in submission.title.lower() and "?" not in submission.title.lower():
                     r.set_flair(subreddit, submission, 'review', 'review')
@@ -96,3 +119,13 @@ while True:
     checkInbox()        
     #Sleep so we abide by the Reddit API rules
     time.sleep(15)
+    #Check times for scheduled giveaways
+    #20 second range on each side to reduce error
+    iterated = -1
+    for giveawayTime in giveawayTimes:
+    iterated += 1
+        if time.time() - 25 <= giveawayTime <= time.time() + 25:
+            GiveawayWinner(giveawayURLs[iterated])
+            #Remove giveaway URL and times so they are not repeated
+            giveawayURLs.remove(0)
+            giveawayTimes.remove(0)
